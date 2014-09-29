3746482a2f4951e544d66379ad2e506e6c8dfa99
Eve Asher <eve.adele.asher@gmail.com>
2014-09-29 13:51:17 -0400

    initial commit

---
+++ lib/rack/prxauth.rb
@@ -0,0 +1,7 @@
+require "rack/prxauth/version"
+
+module Rack
+  module Prxauth
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/prxauth/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Prxauth
+    VERSION = "0.0.1"
+  end
+end
