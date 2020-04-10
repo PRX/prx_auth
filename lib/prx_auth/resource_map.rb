07e96ba1d72b93ff855c15705eaaef2a55b6a7fb
Chris Quamme Rhoden <carhoden@gmail.com>
2020-04-10 13:06:22 -0400

    Spec and implement ResourceMap

--- lib/prx_auth/resource_map.rb
+++ lib/prx_auth/resource_map.rb
@@ -1,4 +1,32 @@
 module PrxAuth
   class ResourceMap
+    WILDCARD_KEY = '*'
+
+    def initialize(mapped_values)
+      @map = Hash[mapped_values.map do |(key, values)|
+        [key, ScopeList.new(values)]
+      end]
+    end
+
+    def contains?(resource, scope=nil)
+      mapped_resource = @map[resource.to_s]
+      if mapped_resource == wildcard_resource
+        raise ArgumentError if scope.nil?
+      
+        mapped_resource.contains?(scope)
+      elsif mapped_resource && !scope.nil?
+        mapped_resource.contains?(scope) || wildcard_resource.contains?(scope)
+      elsif !scope.nil?
+        wildcard_resource.contains?(scope)
+      else
+        !!mapped_resource
+      end
+    end
+
+    private
+
+    def wildcard_resource
+      @wildcard_resource ||= @map[WILDCARD_KEY] || ScopeList.new('')
+    end
   end
 end
--- test/prx_auth/resource_map_test.rb
+++ test/prx_auth/resource_map_test.rb
@@ -1,5 +1,57 @@
 require 'test_helper'
 
 describe PrxAuth::ResourceMap do
-  
+  let(:map) { PrxAuth::ResourceMap.new(resources) }
+  let(:resources) { {'123' => 'admin one two three', '456' => 'member four five six' } }
+
+  describe '#authorized?' do
+    it 'contains scopes in list' do
+      assert map.contains?(123, :admin)
+    end
+
+    it 'does not include across aur limits' do
+      assert !map.contains?(123, :member)
+    end
+
+    it 'does not require a scope' do
+      assert map.contains?(123)
+    end
+
+    it 'does not match if it hasnt seen the resource' do
+      assert !map.contains?(789)
+    end
+
+    describe 'with wildcard resource' do
+      let(:resources) do
+        {
+          '*' => 'peek',
+          '123' => 'admin one two three',
+          '456' => 'member four five six'
+        }
+      end
+
+      it 'applies wildcard lists to queries with no matching value' do
+        assert map.contains?(789, :peek)
+      end
+
+      it 'does not scan unscoped for wildcard resources' do
+        assert !map.contains?(789)
+      end
+
+      it 'allows querying by wildcard resource directly' do
+        assert map.contains?('*', :peek)
+        assert !map.contains?('*', :admin)
+      end
+
+      it 'treats wildcard lists as additive to other explicit ones' do
+        assert map.contains?(123, :peek)
+      end
+
+      it 'refuses to run against wildcard with no scope' do
+        assert_raises ArgumentError do
+          map.contains?('*')
+        end
+      end
+    end
+  end
 end
