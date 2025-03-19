433a9be1d9a536ee588963c9c50c4240a43c42ff
cavis <ryancavis@gmail.com>
2025-03-19 11:08:41 -0600

    Test fixups for modern ruby

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "prx_auth"
 require "rack/prx_auth"
 require "pry"
-require "pry-byebug"
 
 require "minitest/autorun"
 require "minitest/spec"
